8e024d9217f781a547fc328d416831d1a66deeeb
Steven Haye <sphred5@users.noreply.github.com>
2022-06-18 11:16:14 -0700

    added event search component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import {getFeaturedEvents} from '../dummy-data.js';
 import EventList from '../components/events/event-list.js';
-
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
   return (
