aad0938fc8d5778932240c5e18e37fd6b2b54454
Steven Haye <sphred5@users.noreply.github.com>
2022-06-18 13:00:27 -0700

    added filtering functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,19 @@
 import {getAllEvents} from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
-
+import { useRouter } from 'next/router';
 function Events() {
+  const router = useRouter();
   const events = getAllEvents();
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`; 
+    router.push(fullPath);
+  } 
+  
+  
   return (
     <>
-      <EventsSearch/>
+      <EventsSearch onSearch={findEventsHandler}/>
       <EventList items={events}/>
     </>
   )
