1403f14962eade9589dd5bf98e43a4303329ccb3
Steven Haye <sphred5@users.noreply.github.com>
2022-06-30 07:01:23 -0700

    updated comment route

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,34 @@
+export default function handler(req, res){
+  const eventId = req.query.eventId;
+
+  if (req.methos === "POST"){
+    const {email, name, text} = req.body;
+
+    if(!email.includes("@") 
+    || !name 
+    || name.trim() === "" 
+    || !text 
+    || text.trim() === "") {
+    res.status(422).json({message: "Invalid Input"});
+    return;
+    }
+    const newComment = {
+      id : new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+
+    res.status(201).json({message: "added comment", comment: newComment})
+  }
+  if (req.method === "GET"){
+    const dummyList = [
+      {id: 'c1', name: "steven", text: "my first comment"},
+      {id: 'c2', name: "ron", text: "another comment"},
+      {id: 'c3', name: "dj", text: "a different comment"},
+    ]
+    res.status(200).json({comments : dummyList});
+  }
+}
--- pages/api/events/[eventId]/index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res){
-   if (req.method === "POST"){
-    const reqBody = JSON.parse(req.body);
-    const submittedComment = {eventId : req.query.eventId, ...reqBody};
-    res.status(200).json({data: submittedComment});
-   }
-}
