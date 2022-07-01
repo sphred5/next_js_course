40b315c26b18f7463301a4c00259b7a3601b635e
Steven Haye <sphred5@users.noreply.github.com>
2022-06-30 21:41:39 -0700

    hooked newsletter api route up to mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,13 +1,21 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
-  
-    if(!userEmail || !userEmail.includes("@")){
-      res.status(422).json({message: "invalid email address."})
+
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "invalid email address." })
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({message: 'Signed up!'})
+    const client = await MongoClient.connect(process.env.DB_URL)
+    const db = client.db('newsletter');
+
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
+
+    res.status(201).json({ message: 'Signed up!' })
   }
 }
